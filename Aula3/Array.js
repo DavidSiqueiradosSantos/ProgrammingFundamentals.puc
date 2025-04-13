4d5a10909dc0ee41ca66e813865e138679dd67bb
DavidSiqueiradosSantos <davidsiqueiradossantos556@gmail.com>
2025-04-12 23:39:12 -0300

    FundamentosProgramacao JS

--- Aula3/Array.js
+++ Aula3/Array.js
@@ -1,6 +1,4 @@
 let DiaSemana = ['Segunda-Feira', 'Terça-Feira', 'Quarta-Feira',
              'Quinta-Feira', 'Sexta-Feira'];
 
-console.log(DiaSemana);   
-
-TextDecoderStream
+console.log(DiaSemana);
